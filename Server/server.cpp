4c8845e942c3cfea2127ae7ae61d4068ae5bb684
kawaiipooptato <darrellljr@gmail.com>
2023-03-23 14:50:51 +0800

    udp server exception handling

--- Server/server.cpp
+++ Server/server.cpp
@@ -4,11 +4,13 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <stdexcept>
 
 #define BUFFER_SIZE 512
 #define PORT 1234
 std::string ip = "127.0.0.1";
 int timeoutInSeconds = 60;
+SOCKET serverSocket;
 
 struct GameState {
     int playerX;
@@ -16,129 +18,122 @@ struct GameState {
     // add any other game state variables here
 };
 
+void printLastError(const std::string& message) {
+    int error = WSAGetLastError();
+    std::cerr << message << " Error code: " << error << std::endl;
+}
+
 int main() {
     std::cout << "Starting server..." << std::endl;
 
-    // Initialize Winsock
-    WSADATA wsaData;
-    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (result != 0) {
-        std::cerr << "WSAStartup failed: " << result << std::endl;
-        return 1;
-    }
+    try {
+        // Initialize Winsock
+        WSADATA wsaData;
+        int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+        if (result != 0) {
+            throw std::runtime_error("WSAStartup failed");
+        }
 
-    // Create a UDP socket
-    SOCKET serverSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (serverSocket == INVALID_SOCKET) {
-        std::cerr << "socket failed: " << WSAGetLastError() << std::endl;
+        // Create a UDP socket
+        serverSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+        if (serverSocket == INVALID_SOCKET) {
+            throw std::runtime_error("socket failed");
+        }
+
+        // Bind the socket to a local address and port
+        sockaddr_in serverAddr;
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_addr.s_addr = inet_addr(ip.c_str()); /* use INADDR_ANY to bind to any available ip address*/
+        serverAddr.sin_port = htons(PORT);
+
+        result = bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
+        if (result == SOCKET_ERROR) {
+            throw std::runtime_error("bind failed");
+        }
+
+        // When creating a UDP server, you don't need to listen for incoming connections
+        // You can just start handling clients immediately
+
+        // Container for clients
+        std::vector<std::pair<sockaddr_in, std::chrono::time_point<std::chrono::system_clock>>> clients;
+
+        // Initialize game state
+        GameState gameState;
+        gameState.playerX = 0;
+        gameState.playerY = 0;
+        // add any other game state variables here
+
+        // Enter receive loop
+        while (true) {
+            // Receive a datagram from a client
+            sockaddr_in clientAddr;
+            int clientAddrSize = sizeof(clientAddr);
+            char buffer[BUFFER_SIZE];
+            result = recvfrom(serverSocket, buffer, BUFFER_SIZE, 0, (sockaddr*)&clientAddr, &clientAddrSize);
+            if (result == SOCKET_ERROR) {
+                printLastError("recvfrom failed");
+                throw std::runtime_error("recvfrom failed");
+            }
+
+            // Check if client is already in the list
+            bool clientExists = false;
+            for (auto& client : clients) {
+                if (client.first.sin_addr.s_addr == clientAddr.sin_addr.s_addr) {
+                    clientExists = true;
+                    break;
+                }
+            }
+
+            // If client is not in the list, add it
+            if (!clientExists)
+                clients.push_back(std::make_pair(clientAddr, std::chrono::system_clock::now()));
+
+            // Deserialize the received data into a message
+            int receivedPlayerX, receivedPlayerY;
+            std::memcpy(&receivedPlayerX, &buffer[0], sizeof(receivedPlayerX));
+            std::memcpy(&receivedPlayerY, &buffer[sizeof(receivedPlayerX)], sizeof(receivedPlayerY));
+
+            // Server reconciliation
+            if (receivedPlayerX != gameState.playerX || receivedPlayerY != gameState.playerY)
+            {
+                // If the client's position is different from the server's, update the server's position
+                gameState.playerX = receivedPlayerX;
+                gameState.playerY = receivedPlayerY;
+                // add any other game state reconciliation here
+            }
+
+            // Send game state back to client
+            std::memcpy(&buffer[0], &gameState.playerX, sizeof(gameState.playerX));
+            std::memcpy(&buffer[sizeof(gameState.playerX)], &gameState.playerY, sizeof(gameState.playerY));
+            result = sendto(serverSocket, buffer, sizeof(buffer), 0, (sockaddr*)&clientAddr, clientAddrSize);
+            if (result == SOCKET_ERROR) {
+                printLastError("sendto failed");
+                throw std::runtime_error("sendto failed");
+            }
+
+            // Check for clients that have timed out
+            auto currentTime = std::chrono::system_clock::now();
+            for (auto it = clients.begin(); it != clients.end();) {
+                if (std::chrono::duration_cast<std::chrono::seconds>(currentTime - it->second).count() >= timeoutInSeconds) {
+                    std::cout << "Client timed out: " << inet_ntoa(it->first.sin_addr) << std::endl;
+                    it = clients.erase(it);
+                }
+                else {
+                    ++it;
+                }
+            }
+        }
+
+        // Clean up
+        closesocket(serverSocket);
         WSACleanup();
-        return 1;
     }
-
-    // Bind the socket to a local address and port
-    sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = inet_addr(ip.c_str()); /* use INADDR_ANY to bind to any available ip address*/
-    serverAddr.sin_port = htons(PORT);
-
-    result = bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
-    if (result == SOCKET_ERROR) {
-        std::cerr << "bind failed: " << WSAGetLastError() << std::endl;
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
         closesocket(serverSocket);
         WSACleanup();
         return 1;
     }
 
-    // When creating a UDP server, you don't need to listen for incoming connections
-    // You can just start handling clients immediately
-
-    // Container for clients
-    std::vector<std::pair<sockaddr_in, std::chrono::time_point<std::chrono::system_clock>>> clients;
-
-    // Initialize game state
-    GameState gameState;
-    gameState.playerX = 0;
-    gameState.playerY = 0;
-    // add any other game state variables here
-
-    // Enter receive loop
-    while (true) {
-        // Receive a datagram from a client
-        sockaddr_in clientAddr;
-        int clientAddrSize = sizeof(clientAddr);
-        char buffer[BUFFER_SIZE];
-        result = recvfrom(serverSocket, buffer, BUFFER_SIZE, 0, (sockaddr*)&clientAddr, &clientAddrSize);
-        if (result == SOCKET_ERROR) {
-            std::cerr << "recvfrom failed: " << WSAGetLastError() << std::endl;
-            closesocket(serverSocket);
-            WSACleanup();
-            return 1;
-        }
-
-        // Check if client is already in the list
-        bool clientExists = false;
-        for (auto& client : clients) {
-            if (client.first.sin_addr.s_addr == clientAddr.sin_addr.s_addr) {
-                clientExists = true;
-                break;
-            }
-        }
-
-        // If client is not in the list, add it
-        if (!clientExists)
-            clients.push_back(std::make_pair(clientAddr, std::chrono::system_clock::now()));
-
-        // Deserialize the received data into a message
-        int receivedPlayerX, receivedPlayerY;
-        std::memcpy(&receivedPlayerX, &buffer[0], sizeof(receivedPlayerX));
-        std::memcpy(&receivedPlayerY, &buffer[sizeof(receivedPlayerX)], sizeof(receivedPlayerY));
-
-        // Server reconciliation
-        if (receivedPlayerX != gameState.playerX || receivedPlayerY != gameState.playerY) {
-            std::cout << "Server reconciliation required." << std::endl;
-            // Update the game state to match the received data
-            gameState.playerX = receivedPlayerX;
-            gameState.playerY = receivedPlayerY;
-
-            // Send the updated game state to all clients
-            for (auto& client : clients) {
-                int sendResult = sendto(serverSocket, (char*)&gameState, sizeof(gameState), 0, (sockaddr*)&client.first, sizeof(client.first));
-                if (sendResult == SOCKET_ERROR) {
-                    std::cerr << "sendto failed: " << WSAGetLastError() << std::endl;
-                    closesocket(serverSocket);
-                    WSACleanup();
-                    return 1;
-                }
-            }
-        }
-        else {
-            // Send the current game state to the client
-            int sendResult = sendto(serverSocket, (char*)&gameState, sizeof(gameState), 0, (sockaddr*)&clientAddr, sizeof(clientAddr));
-            if (sendResult == SOCKET_ERROR) {
-                std::cerr << "sendto failed: " << WSAGetLastError() << std::endl;
-                closesocket(serverSocket);
-                WSACleanup();
-                return 1;
-            }
-        }
-
-        // Check if any clients have timed out
-        auto currentTime = std::chrono::system_clock::now();
-        for (auto iter = clients.begin(); iter != clients.end(); ) {
-            auto clientTime = iter->second;
-            auto elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(currentTime - clientTime);
-            if (elapsedTime.count() > timeoutInSeconds) {
-                std::cout << "Client timed out: " << inet_ntoa(iter->first.sin_addr) << std::endl;
-                iter = clients.erase(iter);
-            }
-            else {
-                iter++;
-            }
-        }
-    }
-
-    // Clean up Winsock
-    closesocket(serverSocket);
-    WSACleanup();
     return 0;
 }
